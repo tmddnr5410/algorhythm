9b93a23b1c867947e29b36cc0049abdffa87a907
LeeSeungUk <tmddnr5410@gmail.com>
2023-11-21 11:56:28 +0900

    Boj/binsearch : 10816풀이

--- Boj/Binsearch/#10816.py
+++ Boj/Binsearch/#10816.py
@@ -1,3 +1,7 @@
+import sys
+
+input = sys.stdin.readline
+
 N = int(input())
 card = list(map(int, input().split()))
 M = int(input())
@@ -6,32 +10,31 @@ find = list(map(int, input().split()))
 card = sorted(card)
 
 
-def binSearchFindFirst(cards, start, end, target):
-    while start <= end:
+def binsearch_upper(target, arr):
+    start = 0
+    end = len(arr)
+    while start < end:
         mid = (start + end) // 2
-        if target <= cards[mid]:
-            end = mid - 1
-        elif cards[mid] < target:
+        if arr[mid] <= target:
             start = mid + 1
-    if cards[start] == target:
-        return start
-    else:
-        return -1
+        else:
+            end = mid
+    return start
 
 
-lis = []
+def binsearch_lower(target, arr):
+    start = 0
+    end = len(arr)
+    while start < end:
+        mid = (start + end) // 2
+        if target <= arr[mid]:
+            end = mid
+        else:
+            start = mid + 1
+    return start
 
-for i in range(M):
-    count = 0
-    idx = binSearchFindFirst(card, 0, N - 1, find[i])
 
-    if idx != -1:
-        for i in range(idx, N - 1):
-            count += 1
-            # print(card[i], card[idx])
-            if card[i] != card[idx]:
-                print(count)
-                lis.append(count)
-                break
-    else:
-        lis.append(count)
+for num in find:
+    lower = binsearch_lower(num, card)
+    upper = binsearch_upper(num, card)
+    print(upper - lower, end=" ")
