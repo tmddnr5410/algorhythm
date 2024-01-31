444563f792507dac6978f8058aa1d549e869b025
SeungUk Lee <77141675+tmddnr5410@users.noreply.github.com>
2024-01-31 15:30:48 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1548 ms, Memory: 122228 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,45 +1,50 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-public static void main(String[] args) throws Exception {
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	StringTokenizer st = new StringTokenizer(br.readLine());
-	int N = Integer.parseInt(st.nextToken());
-	int M = Integer.parseInt(st.nextToken());
-	int x1, y1, x2, y2;
-	
-	int[][] map = new int[N+1][N+1];
-	for(int y = 1; y <= N; y++) {
-		st = new StringTokenizer(br.readLine());
-		for(int x = 1; x <= N; x++) {
-			map[y][x] = Integer.parseInt(st.nextToken());
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String temp = br.readLine();
+		StringTokenizer st = new StringTokenizer(temp);
+		int N = Integer.parseInt(st.nextToken()); // 표 크기
+		int M =  Integer.parseInt(st.nextToken()); // 합 범위 갯수
+		int [][] mat = new int[N][N];
+		int [][] prefixSum = new int[N+1][N+1];
+		for (int i =0; i< N; i++) {
+			temp = br.readLine();
+			st = new StringTokenizer(temp);
+			for(int j = 0; j< N; j++) {
+				mat[i][j] = Integer.parseInt(st.nextToken());
+				if (i == 0 && j == 0) {
+					prefixSum[i+1][j+1] = mat[i][j];
+				}else if (j == 0){
+					prefixSum[i+1][j+1] = mat[i][j];
+				}else {
+					prefixSum[i+1][j+1] = prefixSum[i+1][j] + mat[i][j];
+				}
+			}
 		}
-	}
-	
-	for(int y = 1; y <= N; y++) {
-		for(int x = 1; x <= N; x++) {
-			map[y][x] += map[y][x-1];
+		for (int test_case = 0 ; test_case <M ; test_case++) {
+			temp = br.readLine();
+			st = new StringTokenizer(temp);
+			int startX = Integer.parseInt(st.nextToken());
+			int startY = Integer.parseInt(st.nextToken());
+			int endX = Integer.parseInt(st.nextToken());
+			int endY = Integer.parseInt(st.nextToken());
+			
+			int result = 0;
+			for (int i = startX ; i <= endX; i++) {
+				result += prefixSum[i][endY] - prefixSum[i][startY-1];
+			}
+			bw.write(result+"\n");
 		}
-	}
-	
-	for(int y = 1; y <= N; y++) {
-		for(int x = 1; x <= N; x++) {
-			map[y][x] += map[y-1][x];
-		}
+		bw.flush();
 	}
 
-
-	for(int i = 0; i < M; i++) {
-		st = new StringTokenizer(br.readLine());
-		x1 = Integer.parseInt(st.nextToken());
-		y1 = Integer.parseInt(st.nextToken());
-		x2 = Integer.parseInt(st.nextToken());
-		y2 = Integer.parseInt(st.nextToken());
-		
-		System.out.println(map[x2][y2] - map[x2][y1-1] - map[x1-1][y2] + map[x1-1][y1-1]);
-	}
-
-}
 }
