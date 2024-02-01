0d8eb23842603ad6a71ac4900651c145721c8d46
SeungUk Lee <77141675+tmddnr5410@users.noreply.github.com>
2024-02-01 14:30:20 +0900

    [Gold V] Title: 신기한 소수, Time: 900 ms, Memory: 16212 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	private static int N;
+	private static int[] canPrime = {1,3,7,9};
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		backTracking("2",1);
+		backTracking("3",1);
+		backTracking("5",1);
+		backTracking("7",1);
+	}
+	public static void backTracking(String now,int depth) {
+		if(!isPrime(Integer.parseInt(now)))
+			return;
+		
+		if(depth == N) {
+			System.out.println(now);
+			return;
+		}
+		
+		for(int p:canPrime) {
+			backTracking(now+p,depth+1);
+		}
+		
+	}
+	
+	public static boolean isPrime(int num) {
+		for(int i=2;i<num;i++) {
+			if(num%i == 0) 
+				return false;
+		}
+		return true;
+	}
+}
